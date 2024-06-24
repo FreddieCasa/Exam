5ef8042502eeca5fc7e70b85df2053d3bb279445
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-06-24 16:22:29 +0300

    add getters and setters

--- bookrecommendation/src/main/java/com/example/bookrecommendation/model/Book.java
+++ bookrecommendation/src/main/java/com/example/bookrecommendation/model/Book.java
@@ -14,6 +14,36 @@ public class Book {
     private String author;
     private String genre;
     private String description;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
     // getters and setters
 }
